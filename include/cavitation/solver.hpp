d5f123e5298ef4fddcf9cf0ecddd221dc3f46b0d
Rohan Aich <aich.rohan06@gmail.com>
2026-08-07 00:38:01 +0800

    Inital starter files commit, including .git-ignore

---
+++ include/cavitation/solver.hpp
@@ -0,0 +1,12 @@
+//
+// Created by Rohan_Aich on 6/8/2026.
+//
+
+#pragma once
+
+namespace cavitation {
+    class Solver {
+        public:
+            void run();
+    };
+}
---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Rohan_Aich on 6/8/2026.
+//
+
+#include <cavitation/solver.hpp>
+
+int main() {
+    cavitation::Solver solver;
+    solver.run();
+
+    return 0;
+}
